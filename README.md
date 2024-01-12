a87072d6b2ecfec21db9e05e87c47cd3b8e6811e
nbiancolin <117390343+nbiancolin@users.noreply.github.com>
2024-01-12 00:24:41 -0500

    files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # review sentiment detector
  A Machine Intelligence program that detects if a review is positibe or negative
+
+ Built using Visual Studio and ML.NET
+
+ ## Running Instructions
+ (using Visual Studio) open the project and press ctrl + f5 to build and run the program
---
+++ myMLApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyMLApp;
+// Add input data
+Console.WriteLine("Enter your review: ");
+string review = Console.ReadLine();
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = review
+};
+
+// Load model and predict output of sample data
+var result = SentimentModel.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
